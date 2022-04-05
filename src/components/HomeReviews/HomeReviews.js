7d419d8eb93666b4347ca8c50040c51479c47c7f
mozammelhoq1 <mozammelhoq512@gmail.com>
2022-04-05 10:00:28 +0600

    reviews part complete

--- src/components/HomeReviews/HomeReviews.js
+++ src/components/HomeReviews/HomeReviews.js
@@ -1,14 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Container, Row } from "react-bootstrap";
+import useReviews from "../../hooks/useReviews";
 import Review from "../Review/Review";
 
 const HomeReviews = () => {
-  const [reviews, setReviews] = useState([]);
-  useEffect(() => {
-    fetch("review.json")
-      .then((res) => res.json())
-      .then((data) => setReviews(data));
-  }, []);
+  const [reviews] = useReviews();
   return (
     <Container>
       <Row xs={1} md={2} lg={3} xl={4} className="g-4">
--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -1,15 +1,27 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
 import React from "react";
 import { Card, Col } from "react-bootstrap";
 
 const Review = ({ review }) => {
-  const { id, name } = review;
+  const { name, img, comment, rating } = review;
   return (
     <Col>
-      <Card className="align-items-center pb-2 border-0 shadow rounded-3">
-        {/* <Card.Img variant="top" src={strMealThumb} /> */}
+      <Card className="justify-content-center align-items-center border-0 shadow rounded-3 h-100  py-3">
+        <Card.Img className="rounded-circle w-50" variant="top" src={img} />
         <Card.Body>
-          <Card.Title>{id}</Card.Title>
-          <Card.Text>{name}</Card.Text>
+          <Card.Title>
+            <small className="fw-bold me-3">{name}</small>{" "}
+            <small className="text-warning me-2">
+              <FontAwesomeIcon icon={faStar} />
+            </small>
+            <small className="text-secondary">{rating}</small>
+          </Card.Title>
+          <Card.Text>
+            <blockquote className="blockquote-footer mt-3">
+              {comment}
+            </blockquote>
+          </Card.Text>
         </Card.Body>
       </Card>
     </Col>
